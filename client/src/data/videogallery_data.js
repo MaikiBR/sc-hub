d54de3eb4c5e80a2f6f86487a1ec2e175fc4a428
Miguel Ángel Bermea Rdz <a01411671@itesm.mx>
2023-02-04 23:15:22 -0600

    Videos on Firebase, PENDING & DETAILS in desc
    PENDING & DETAILS:
    + Add user in Admin manager
       - Test endpoint functionality
    + List users in Admin manager
      - Results column in datagrid
      - Delete user implementation
      - Update user implementation
    + Videoteca cards fixed
    - Evaluations section
    - Login with db users
    - Puestos y sucursales from db

--- client/src/data/videogallery_data.js
+++ client/src/data/videogallery_data.js
@@ -1,21 +1,3 @@
-// #region Video Media
-// import videoValores from '../assets/videos/Procesos/Administracion de valores.mp4'
-// import videoCobro from '../assets/videos/Procesos/Cobro.mp4'
-// import videoPasteleria from '../assets/videos/Procesos/Pasteleria.mp4'
-// import videoPedidoEsp from '../assets/videos/Procesos/PEDIDO ESPECIAL.mp4'
-// import videoReservacion from '../assets/videos/Procesos/RESERVACION.mp4'
-// import videoServCliente from '../assets/videos/Procesos/Servicio al cliente.mp4'
-
-// import videoInduccion from '../assets/videos/Institucionales/VideoInducción.mp4'
-// import videoSegPrimero from '../assets/videos/Institucionales/1 LA SEGUR1 IDAD LA HACEMOS TODOS g.mp4'
-
-// import videoSec1 from '../assets/videos/Seguridad/2 PREVENCION DE QUEMADURAS GSC.mp4'
-// import videoSec2 from '../assets/videos/Seguridad/3 LEVANTAMIENTO MANUAL DE CARGAS G.mp4'
-// import videoSec3 from '../assets/videos/Seguridad/5 PREVENCION DE CORTADURAS G.mp4'
-// import videoSec4 from '../assets/videos/Seguridad/4 PREVENCION DE CAIDAS G.mp4'
-
-// #endregion
-
 // #region Thumbnail Media
 // INSTITUCIONAL
 import thumbInduccion from "../assets/images/thumbnails/Material Video RH Titulos 1600x900px_Mesa de trabajo 1.jpg"
@@ -41,32 +23,32 @@ export const videoPData = [
     {
         id: 1,
         thumbnail: thumbValores,
-        // video: videoValores
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/Administracion%20de%20valores.mp4?alt=media&token=dba16bdb-452d-4f57-9fad-a26103b602df"
     },
     {
         id: 2,
         thumbnail: thumbCobro,
-        // video: videoCobro
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/Cobro.mp4?alt=media&token=e81edf69-675c-40f9-af1a-c2c6f8235ae8"
     },
     {
         id: 3,
         thumbnail: thumbPasteleria,
-        // video: videoPasteleria
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/Pasteleria.mp4?alt=media&token=2feb51f7-b57a-474e-8ebf-052d83e90d97"
     },
     {
         id: 4,
         thumbnail: thumbPedEsp,
-        // video: videoPedidoEsp,
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/PEDIDO%20ESPECIAL.mp4?alt=media&token=b01cf378-bcb6-4b73-8fcd-21e8f02f9101"
     },
     {
         id: 5,
         thumbnail: thumbReserv,
-        // video: videoReservacion,
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/RESERVACION.mp4?alt=media&token=e64281bb-3933-4480-9238-a63fe9d1b1ab"
     },
     {
         id: 6,
         thumbnail: thumbServC,
-        // video: videoServCliente,
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/Servicio%20al%20cliente.mp4?alt=media&token=dba22348-d69f-4a90-9add-ff15460a96d7"
     },
 ];
 
@@ -74,22 +56,22 @@ export const videoSData = [
     {
         id: 1,
         thumbnail: thumbSegQuemaduras,
-        // video: videoSec1
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/2%20PREVENCION%20DE%20QUEMADURAS%20GSC.mp4?alt=media&token=126a37cf-f102-4116-a06c-e23b8c8b3668"
     },
     {
         id: 2,
         thumbnail: thumbSegCargas,
-        //video: videoSec2
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/3%20LEVANTAMIENTO%20MANUAL%20DE%20CARGAS%20G.mp4?alt=media&token=a8535967-0f9c-447c-b210-1529316549cb"
     },
     {
         id: 3,
         thumbnail: thumbSegCortaduras,
-        //video: videoSec4
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/5%20PREVENCION%20DE%20CORTADURAS%20G.mp4?alt=media&token=baaec5eb-f1bd-4eab-9317-9eac5b4adf23"
     },
     {
         id: 4,
         thumbnail: thumbSegCaidas,
-        //video: videoSec3
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/4%20PREVENCION%20DE%20CAIDAS%20G.mp4?alt=media&token=c96edf81-8e30-4028-9200-80de3e2a7a14"
     },
     
 ];
@@ -98,11 +80,12 @@ export const videoIData = [
     {
         id: 1,
         thumbnail: thumbInduccion,
-        //video: videoInduccion
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/VideoInducción.mp4?alt=media&token=54ee4e80-d30e-47e3-a23e-a257f6fb2db2"
+
     },
     {
         id: 2,
         thumbnail: thumbSegInstitucional,
-        //video: videoSegPrimero
+        video: "https://firebasestorage.googleapis.com/v0/b/hr-hub-66f11.appspot.com/o/1%20LA%20SEGUR1%20IDAD%20LA%20HACEMOS%20TODOS%20g.mp4?alt=media&token=57a8b34c-9274-4d97-94db-666a04a09aef"
     },
 ];
